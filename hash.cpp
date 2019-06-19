471d2a6043da44b286a5a61140758bdd5cae4a4c
adrianjmejias <adrianjmejias@gmail.com>
2019-06-19 10:22:49 -0400

    cambios en hash.cpp falta generar random

---
+++ hash.cpp
@@ -0,0 +1,73 @@
+
+
+
+
+
+
+#include <vector>
+
+#include <map>
+#include <unordered_map>
+
+
+
+std::vector<int> GenerateRandom(size_t size, std::function<int()> generator){
+    std::vector<int> randVec;
+    for(int ii= 0; ii< size; ii++){
+        randVec.push_back(generator());
+    }
+    return randVec;
+}
+
+int main(){
+
+    std::vector<int> testCases;
+    std::unordered_map<int> _umap;
+    std::map<int> _map;
+
+
+    for(auto test : testCases){
+        auto vectorTest = GenerateRandom(test, [](){
+            int randInt;
+
+            // Generar Random
+
+
+            return randInt;
+        });
+
+
+        
+        {
+            size_t iiSize = _map.size() -1;
+    
+            for(auto &b : testCases){
+                _map.insert(b);
+            }
+            
+            for(; iiSize>= 0; iiSize--){
+                _map.erase(testCases[ii]);
+            }
+        }
+    
+    
+        
+        {
+            size_t iiSize = _umap.size() -1;
+            
+            for(auto b : testCases){
+                _umap.insert(b);
+            }
+            
+            for(; iiSize>= 0; iiSize--){
+                _umap.erase(testCases[ii]);
+            }
+        }
+    }
+
+
+
+
+
+
+}
