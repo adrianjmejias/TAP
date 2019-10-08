6f8699337398ee0ac7ac53fe0da005cda855dfba
adrianjmejias <adrianjmejias@gmail.com>
2019-10-08 01:46:00 -0400

    problem done with class

--- foxlings.cpp
+++ foxlings.cpp
@@ -1,33 +1,113 @@
 #include <unordered_map>
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
-using ull = unsigned long long;
+
+using ull = int;
 
 std::unordered_map<ull, ull> foxlings;
 
-ull root(ull kid)
+
+// •Implementar la clasede conjuntos disjuntos. Incluir constructor, find y union-pertenece.
+
+
+
+void print(std::vector<ull> a)
 {
-    if(!foxlings[kid])
+    std::cout<< std::endl;
+
+    for (int ii = 0; ii < a.size(); ii++)
     {
-        foxlings[kid] = kid;
+        std::cout << a[ii] <<", ";
+
+    }
+    std::cout<< std::endl;
+    
+    for (int ii = 0; ii < a.size(); ii++)
+    {
+        std::cout << ii <<", ";
+
     }
 
-	while(kid != foxlings[kid])
-    {
-        foxlings[kid] = foxlings[foxlings[kid]];
-        kid = foxlings[kid];
-    }
+    std::cout<< std::endl;
 
-    return kid;
+    
+
 }
+
+
+
+class DisjointSet 
+{
+    public:
+    std::vector<ull> data;
+
+    DisjointSet(ull size)
+    {
+        data.reserve(size);
+        std::fill_n(std::back_inserter(data), size, -1);
+    }
+
+
+    // Find(x): devuelve la raíz del árbol al cual
+    // pertenece x
+    ull find(ull index)
+    {
+        while(index != data[index]) index = data[index];
+        return index;
+    }
+
+    // Unión-pertenencia: la operación de
+    // pertenencia busca la raíz de los árboles en
+    // donde están ambos elementos. Si la raíz
+    // coincide retorna verdad. En caso contrario
+    // falso. El parámetro unión indica si los
+    // conjuntos deben unirse.
+    bool union_belong(int x, int y, bool doUnion)
+    {
+        ull rx = find(x);
+        ull ry = find(y);
+
+
+        // std::cout<< "x "<< x <<" rx "<< rx <<std::endl;
+        // std::cout<< "y "<< y <<" ry "<< ry <<std::endl;
+        bool were_joined = rx == ry;
+
+        if(!were_joined && doUnion)
+        {
+            data[rx] = ry;
+            were_joined=true;
+            // std::cout<<"joined " << rx << " to "<< ry<<std::endl;
+        }
+        
+        
+
+        return were_joined;
+    }
+
+    void add(ull toInsert)
+    {
+        // while(toInsert >= data.size()) data.push_back(-1);
+        bool isNew = data[toInsert] < 0;
+        // std::cout<< toInsert <<" isNew "<< isNew << std::endl;
+
+        // if(isNew)
+        {
+            data[toInsert] = toInsert;
+        }
+
+    }
+};
 
 int main()
 {
     ull res =0, n, m;
 
-
-
     std::cin >> n >> m;
+
+    DisjointSet foxies(n+2);
+
 
     res = n;
     while(m--)
@@ -36,19 +116,19 @@ int main()
 
         std::cin >> a >> b;
 
-        ull pa = root(a);
-        ull pb = root(b);
+        foxies.add(a);
+        foxies.add(b);
 
-        if( pa != pb)
+        if(foxies.union_belong(a,b, true))
         {
+            // std::cout<<"res--"<<std::endl;
             res--;
-            foxlings[pb]= pa;
         }
 
     }
 
-
-    std::cout << res<< std::endl;
+    // print(foxies.data);
+    std::cout << res << std::endl;
 
     return 0;
 }
