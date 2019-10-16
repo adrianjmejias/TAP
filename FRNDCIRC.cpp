329c4b0029bf81b950555216099b397d872e8ed9
adrianjmejias <adrianjmejias@gmail.com>
2019-10-15 22:04:51 -0400

    class

--- FRNDCIRC.cpp
+++ FRNDCIRC.cpp
@@ -9,8 +9,67 @@
 
 #include <vector>
 #include <string>
-#include <map>
+#include <unordered_map>
 #include <iostream>
+
+using std::unordered_map;
+using std::endl;
+using std::cout;
+using std::cin;
+using std::string;
+
+
+#define tcout(x) 
+
+
+
+template <typename TKey>
+class DisjointSet : protected unordered_map<TKey, TKey>{
+
+    public:
+
+        bool union_belong(TKey a, TKey b, bool shouldUnion)
+        {
+            TKey pa = find(a);
+            TKey pb = find(b);
+
+            tcout( << "pa " << pa<<"   a   " << a << endl);
+            tcout( << "pb " << pb<<"   b   " << b << endl);
+
+            bool belong = pa == pb;
+            if(!belong && shouldUnion)
+            {
+                (*this)[pb] = pa;
+                belong = true;
+            }
+
+            return belong;
+        }
+
+        void add(TKey a)
+        {
+            (*this)[a] = a;
+        }
+
+
+        TKey find(TKey child)
+        {
+
+            tcout( << "find  " << child <<endl);
+            if(child == (*this)[child])
+            {
+                tcout( << "return " << child<< endl);
+
+                return child;
+            }
+
+            TKey parent = find((*this)[child]);
+            (*this)[child] = parent;
+            tcout( << "return " << parent<< endl);
+            return parent;
+        }
+};
+
 
 int main()
 {
@@ -20,16 +79,53 @@ int main()
     // Each test case begins with a line containing an integer N (N<=100000), the number of new relations.
     // Each of the following N lines contains couple of strings denoting the names of two people who have just formed relation,
     // separated by a space. Names will have no more than 20 characters.
+    
+
+
+    // DisjointSet<string> test;
+
 
     int test;
 
-    std::cin >> test;
+    cin >> test;
 
     while(test--)
     {
         int N;
+        cin >> N;
+        
+        DisjointSet<string> crc;
+        unordered_map<string, int> qt;
+        for(string a,b; N > 0 ; N--)
+        {
+            cin >> a >> b;
 
-        std::cin >> N;
+
+            if(!qt[a])
+            {
+                qt[a] = 1;
+                crc.add(a);
+            }
+
+            if(!qt[b])
+            {
+                qt[b] = 1;
+                crc.add(b);
+            }
+
+
+            string pa;
+            string pb;
+            pa = crc.find(a);
+            pb = crc.find(b);
+
+            if(crc.union_belong(a,b, true))
+            {
+                qt[pa] = qt[pa] + qt[pb];
+            }
+
+            cout << qt[pa] << endl;
+        }
         
     }
 
